fa67c4a15b094b7d234cbcf13c0644038c95fa03
raymondlwb <jasonlee1981@gmail.com>
2017-12-23 15:50:54 +0800

    Update DemoService.java

--- src/main/java/spring/demo/service/DemoService.java
+++ src/main/java/spring/demo/service/DemoService.java
@@ -8,9 +8,9 @@ public class DemoService {
 	public String Verify(String username,String password){
 		
 		if(username.equals("admin") && password.equals("admin")){
-			return "Login Success！";
+			return "Login Success！after add";
 		}else{
-			return "Login Failed,Please Try Again！";
+			return "Login Failed,Please Try Again！after add";
 		}
 	}
 }
