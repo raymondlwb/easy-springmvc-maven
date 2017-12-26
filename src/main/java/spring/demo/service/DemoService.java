ea349e3521df038cd8f3b45e093da73e5fa43fc4
raymondlwb <jasonlee1981@gmail.com>
2017-12-26 15:22:27 +0800

    Update DemoService.java

--- src/main/java/spring/demo/service/DemoService.java
+++ src/main/java/spring/demo/service/DemoService.java
@@ -8,9 +8,9 @@ public class DemoService {
 	public String Verify(String username,String password){
 		
 		if(username.equals("admin") && password.equals("admin")){
-			return "Login Success！after add webhook222";
+			return "Login Success！after add webhook and secrure key on github";
 		}else{
-			return "Login Failed,Please Try Again！after add webhook222";
+			return "Login Failed,Please Try Again！after add webhook and secrure key on github";
 		}
 	}
 }
